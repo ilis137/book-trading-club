86f2a58f1ae56df995fd5764e4ecabf13a7215b2
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-01 09:08:13 +0530

    add express session

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,54 @@
-const passport = require("passport")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const githubStrategy = require("passport-github").Strategy;
+const mongoose = require("mongoose")
 
-module.exports = app => {
 
-    app.get("/auth/github", passport.authenticate("github"))
+require("dotenv").config();
+const User = require("./models/users")
 
-    app.get("/auth/github/callback", passport.authenticate("github", { successRedirect: "/books", failureRedirect: "/login" }))
-}
+console.log(process.env.CLIENT_ID)
+console.log(process.env.CLIENT_SECRET)
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Apply Middleware
+app.use(cors());
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.mongoURL, { useNewUrlParser: true }, () => {
+        console.log("successfully connected to mongodb")
+    })
+    //Authentication Middleware
+passport.use(new githubStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "https://book-trading-iliyas.herokuapp.com/auth/github/callback"
+}, (accessToken, refreshToken, profile, cb) => {
+    console.log(profile)
+    User.findOrCreate({ username: profile.username, fullname: profile.displayName }).then((err, user) => {
+        console.log(user)
+        cb(err, user)
+    })
+}));
+//serialize session
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+//deserialize se
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+require("./routes/routes")(app)
+    //Start the server
+app.listen(port, () => console.log(`server is up at port ${port}`));
--- server.js
+++ server.js
@@ -17,14 +17,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Apply Middleware
+app.use(require('express-session')({ secret: "hsdfgmsh234sd", resave: true, saveUninitialized: true, maxAge: 24 * 60 * 1000 }));
 app.use(cors());
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(require('express-session')({ secret: "hsdfgmsh234sd", resave: true, saveUninitialized: true, maxAge: 24 * 60 * 1000 }));
-
 
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.mongoURL, { useNewUrlParser: true }, () => {
@@ -46,7 +45,7 @@ passport.use(new githubStrategy({
 passport.serializeUser((user, done) => {
     done(null, user);
 });
-//deserialize session
+//deserialize se
 passport.deserializeUser((obj, done) => {
     done(null, obj);
 });
