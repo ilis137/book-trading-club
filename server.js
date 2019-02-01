90efc0672680edf7938b164f420e323965df94ea
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-01 08:39:33 +0530

    connect to mongodb

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const githubStrategy = require("passport-github").Strategy;
+const mongoose=require("mongoose")
+
 
 require("dotenv").config();
 const User = require("./models/users")
@@ -22,6 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+mongoose.Promise=global.Promise
+mongoose.connect(process.env.mongoURL, { useNewUrlParser: true }, () => {
+    console.log("successfully connected to mongodb")
+})
 //Authentication Middleware
 passport.use(new githubStrategy({
     clientID: process.env.CLIENT_ID,
