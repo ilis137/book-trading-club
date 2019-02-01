5eaf877dda8900f47908801b43373ac600cdba68
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-01 08:54:56 +0530

    add find or create

--- models/users.js
+++ models/users.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose")
-
+const findOrCreate = require("mongoose-findorcreate")
 const userSchema = new mongoose.Schema({
     username: String,
     fullname: String,
     city: String,
     address: String
 })
+
+userSchema.plugin(findOrCreate)
+
 const userModel = new mongoose.model("user", userSchema);
 module.exports = userModel
