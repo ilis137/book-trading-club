9add26026b55eabc1d49ff592534e2f49f89ca97
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-02 06:13:47 +0530

    API to  work on userInfo

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,39 @@
 const passport = require("passport")
-
+const User = require("../models/users")
 module.exports = app => {
 
     app.get("/auth/github", passport.authenticate("github"))
 
     app.get("/auth/github/callback", passport.authenticate("github", { successRedirect: "/books", failureRedirect: "/login" }))
+
+    //API to Edit user Info
+    app.put("/editInfo", (req, res) => {
+            const { username, fullname, city, address } = req.body
+            User.findOneAndUpdate({ username: username }, { username: username, fullname: fullname, city: city, address: address }, { new: true }).then((user, err) => {
+                if (err)
+                    throw (err)
+                else
+                    res.redirect("/userInfo")
+            })
+        })
+        //API to get info for Editing profile
+    app.get("/editInfo", (req, res) => {
+        User.findOne({ username: username }).then((user, err) => {
+            if (err)
+                throw (err)
+            else
+                res.render("editInfo", { username: username, fullname: fullname, city: city, address: address })
+        })
+    })
+
+    //API to get userInfo
+    app.get("/UserInfo", require("connect-ensure-login").ensureLoggedIn(), (req, res) => {
+        User.findOne({ username: username }).then((user, err) => {
+            if (err)
+                throw (err)
+            else
+                res.render("userInfo", { username: username, fullname: fullname, city: city, address: address })
+        })
+    })
+
 }
