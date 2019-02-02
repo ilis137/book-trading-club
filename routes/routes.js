c58add1c81f85d19c1bd9f66adbdb5942b407746
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-02 07:42:20 +0530

    Add api to deal with books

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const passport = require("passport")
 const User = require("../models/users")
+const Book = require("../models/books")
 module.exports = app => {
 
     app.get("/auth/github", passport.authenticate("github"))
@@ -37,4 +38,60 @@ module.exports = app => {
         })
     })
 
+
+
+    //API to add a book for trade
+    app.post("/books/my", (req, res) => {
+        const { username } = req.user
+        const { title, author } = req.body
+        const book = new Book({
+            title: title,
+            author: author,
+            ownersname: username
+        })
+
+        Book.save().then(book => {
+            res.redirect("/books/my")
+        }).catch(err => {
+            throw (err)
+        })
+    })
+
+    //API to get my books for trade
+    app.get("/books/my", (req, res) => {
+        const { username } = req.user
+        Book.find({ ownersname: username }).then((books) => {
+            res.render("addBook", { books: books })
+        }).catch(err => {
+            throw (err)
+        })
+
+    })
+
+    //API to delete a book for trade
+    app.delete("/books/my", (req, res) => {
+            const { username } = req.user
+            const { title } = req.body
+
+            Book.findOneAndDelete({ ownersname: username, title: title }).then(() => {
+                res.redirect("/books/my")
+            }).catch(err => {
+                throw (err)
+            })
+        })
+        //API to get all books for trade
+    app.get("/books", (req, res) => {
+
+            Book.find({}).then((books) => {
+                User.find({ username: username }).then(user => {
+                    res.render("Books", { books: books, ownersname: user.username, city: user.city })
+                }).catch(err => { throw (err) })
+
+            }).catch(err => {
+                throw (err)
+            })
+        })
+        //API 
+
+    // app.get("/")
 }
