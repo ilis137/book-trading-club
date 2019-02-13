f96c176e045d1cf65c936cd75e29c2e0a9044d7e
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-13 08:03:08 +0530

    fix typo

--- routes/routes.js
+++ routes/routes.js
@@ -261,7 +261,7 @@ module.exports = app => {
   //API to delete my request
   app.delete("/delete/request", (req, res) => {
     const { id } = req.body;
-    Request.findByIdAndDelete(id)
+    Request.findByIdAndRemove(id)
       .then(() => {
         res.redirect("/createRequests");
       })
