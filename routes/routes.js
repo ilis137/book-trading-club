8fa5e4ff2d9ad687e92d1fee84bfbd2e960f465e
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-13 07:53:53 +0530

    remove request

--- routes/routes.js
+++ routes/routes.js
@@ -279,7 +279,7 @@ module.exports = app => {
   //API to cancel request
   app.post("/cancel/request", (req, res) => {
     const { id } = req.body;
-    Request.findByIdAndUpdate(id, { status: "canceled" }, { new: true })
+    Request.findByIdAndRemove(id)
       .then(() => {
         res.redirect("/books");
       })
